6e7ae1d09a51338e808497edc8264b53a3bcb797
toolbob01 <jilehook@gmail.com>
2022-08-08 21:28:13 +0900

    push push baby

--- src/main/java/com/hellobook/domain/MemberVO.java
+++ src/main/java/com/hellobook/domain/MemberVO.java
@@ -1,5 +1,6 @@
 package com.hellobook.domain;
 
+import java.util.Date;
 import java.util.List;
 
 import lombok.Data;
@@ -10,17 +11,10 @@ public class MemberVO {
 	private String email;
 	private String nickname;
 	private String password;
-	
-	private String intro;
 	private Date birth;
-	
-	private char language;  // 'k' 'j'
-	private char sex;       // 'm' 'f'
+	private char language; 
+	private char sex;       
 	private String hobby;
-<<<<<<< HEAD
+
 	private List<AuthVO> authList;
-=======
-	
-	private String profile;
->>>>>>> 41195442a0d0e6ed93edb2a2ff9bb23e42275278
 }
