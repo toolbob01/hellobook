92d33ec691a8191704d2882b815084c84ca60a93
IMJUNWOO <kumosun1@gmail.com>
2022-08-10 20:52:52 +0900

    script 정리

---
+++ src/main/webapp/resources/js/findpwdService.js
@@ -0,0 +1,59 @@
+var findPwdService = (function() {
+	function checkEmail(email, callback, err) {
+		console.log("되긴함?")
+		var data = {
+				email:email
+		} 
+		$.ajax({
+			type:"post",
+			url:"/findpwd/checkemail",
+			data:JSON.stringify(data),
+			contentType:'application/json; charset = utf-8',
+			success: function(result,status,xhr) {
+				if(callback){
+					callback(result);
+				}
+			},error: function(xhr,status,er){
+				if(err){
+					err();
+				}
+			}
+		})
+	}
+	
+	function getCode(email,callback,err) {
+		$.ajax({
+			type : "get",
+			url : "/findpwd/getcode?email=" + email,
+			success : function(result){
+				if(callback){
+					callback(result);
+				}
+			},error: function(xhr,status,er){
+				if(err){
+					err();
+				}
+			}
+		})
+	}
+	
+	function changePwd(userData,callback,err) {
+		$.ajax({
+			type:'post',
+			url:'/findpwd/changePwd',
+			data:JSON.stringify(userData),
+			contentType : "application/json",
+			success:function(result){
+				if(callback){
+					callback(result);
+				}
+			},error: function(xhr,status,er){
+				if(err){
+					err();
+				}
+			}
+		})
+	}
+
+	return {checkEmail:checkEmail, getCode:getCode, changePwd:changePwd};
+})();
