443010099315beb682a644a6b02ec2a7cf30cace
IMJUNWOO <kumosun1@gmail.com>
2022-08-20 15:13:45 +0900

    quit

--- src/main/java/com/hellobook/controller/MypageController.java
+++ src/main/java/com/hellobook/controller/MypageController.java
@@ -10,20 +10,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.deser.DataFormatReaders.Match;
 import com.hellobook.domain.MemberVO;
 import com.hellobook.domain.PostVO;
 import com.hellobook.domain.ReplyVO;
 import com.hellobook.domain.SessionVO;
+import com.hellobook.mapper.MemberMapper;
 import com.hellobook.service.MemberService;
 import com.hellobook.service.PostService;
 import com.hellobook.utility.Time;
@@ -37,6 +44,9 @@ public class MypageController {
 	
 	@Autowired
 	private PostService postService;
+	
+	@Autowired
+	private BCryptPasswordEncoder pwencoder;
 	
 	@GetMapping("/unknown")
 	public String unknown() {
@@ -212,11 +222,25 @@ public class MypageController {
 	       
 	        return "/mypage/setting/quit";
 	    }
+	  
+	  @PostMapping(value="/setting/quit", consumes = "application/json", produces = {MediaType.TEXT_PLAIN_VALUE})
+	  public ResponseEntity<String> removeEmail(@RequestBody MemberVO mvo) {
+		  ResponseEntity<String> result = null;
+		  
+		  String putInPwd = mvo.getPassword();
+		  SessionVO svo = memberService.read(mvo.getEmail());
+		  
+		  if(pwencoder.matches(putInPwd, svo.getPassword())) {
+			  memberService.quitMember(mvo.getEmail());
+			  
+			  result = new ResponseEntity<String>("0",HttpStatus.OK);
+		  }else{
+			  result = new ResponseEntity<String>("1",HttpStatus.OK);
+		  }
+//		  
+		  return result != null? result : new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+	  }
 
-
-	
-	
-	
 	
 	@GetMapping("/setting/report")
 	public String report() {
