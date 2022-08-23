e2c7825bd0018de93848dd31d39e1b7c779a2fc6
toolbob01 <jilehook@gmail.com>
2022-08-23 22:28:53 +0900

    Create Chat Room

--- src/main/java/com/hellobook/mapper/ChatMapper.java
+++ src/main/java/com/hellobook/mapper/ChatMapper.java
@@ -1,5 +1,16 @@
 package com.hellobook.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.hellobook.domain.ChatVO;
+
 public interface ChatMapper {
+	
+	public void createChatRoom(@Param("email") String email, @Param("femail")String femail);
+	
+	public List<ChatVO> chatRoomList(String email);
 
 }
+
